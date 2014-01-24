06447e4cce6c758ef55c5e33b761c0ff1908642e
Emilien-Antoine <emilientoine@gmail.com>
2014-01-24 11:54:44 +0100

    update test suite

--- test/ts_webdamlog.rb
+++ test/ts_webdamlog.rb
@@ -46,6 +46,7 @@ end
 if ARGV.include?("ordered")
   ARGV.delete("ordered")
   p "test suite in order"
+  require "30_provenance/tc_wl_deletion.rb"
   require "30_provenance/tc_wl_trace_push_out.rb"
   require "10_first_gen/tc_wl_wlbud_dynamic.rb"
   require "10_first_gen/tc_wl_runner.rb"
@@ -55,6 +56,12 @@ if ARGV.include?("ordered")
   require "10_first_gen/tc_wl_wlbud_parse_program.rb"
   require "10_first_gen/tc_wl_program_treetop.rb"
   require "10_first_gen/tc_wl_wlbud_delay_load_fact.rb"
+  require "20_deleg_seeds/tc_wl_program.rb"
+  require "20_deleg_seeds/tc_wl_seed.rb"
+  require "20_deleg_seeds/tc_wl_four_peers_program.rb"
+  require "20_deleg_seeds/tc_wl_pending_delegations.rb"
+  require "20_deleg_seeds/tc_peer_vars.rb"
+  require "20_deleg_seeds/tc_wl_selfjoin_rewriting.rb"
   require "01_bud/tc_bud_collection.rb"
   require "01_bud/tc_bud_delete_fact.rb"
   require "00_misc/tc_project_conf.rb"
@@ -69,12 +76,6 @@ if ARGV.include?("ordered")
   require "02_dumb/tc_wl_wlbud_callback.rb"
   require "02_dumb/tc_wl_wlbud_delegation_2_complex.rb"
   require "02_dumb/tc_wl_wlbud_send_packet.rb"
-  require "20_deleg_seeds/tc_wl_program.rb"
-  require "20_deleg_seeds/tc_wl_seed.rb"
-  require "20_deleg_seeds/tc_wl_four_peers_program.rb"
-  require "20_deleg_seeds/tc_wl_pending_delegations.rb"
-  require "20_deleg_seeds/tc_peer_vars.rb"
-  require "20_deleg_seeds/tc_wl_selfjoin_rewriting.rb" 
 else
   files.each { |file| require file }
   

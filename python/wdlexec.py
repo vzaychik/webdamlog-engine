42ae83f03e9ec4957aa582ac9c429720f0ca2149
miklau <miklau@cs.umass.edu>
2014-03-14 22:04:01 -0400

    remove debug

--- python/wdlexec.py
+++ python/wdlexec.py
@@ -96,7 +96,6 @@ def run(configFile):
                 boolString = tup[0]
                 accessBool = bool( int(boolString[0]))
                 optim1Bool = bool( int(boolString[1]))
-                print 'Flags are: ', accessBool, optim1Bool
                 execID = execution.executeScenario( rootPath, scenID, scenType, accessBool, optim1Bool, config.getint('execution', 'ticks'),  \
                                  config.getfloat('execution', 'sleep'), config.getfloat('execution', 'masterDelay')   )
                 print '***  Finished run %i of execution %i.' % (run, execID)

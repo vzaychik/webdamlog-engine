519f35508cac1b812fc72f480cb2eb64f29a656e
WebdamLog Experiment User <webdam@waltz.cs.drexel.edu>
2015-08-19 15:06:07 -0400

    Changed to only reestablish network connection on unbind if there are messages left to send

--- lib/wlbudoverride/collections.rb
+++ lib/wlbudoverride/collections.rb
@@ -6,8 +6,6 @@ module Bud
     def establish_connection(l)
       toplevel = @bud_instance.toplevel
       toplevel.connections[l] = EventMachine::connect l[0], l[1], ConnectionClient, @bud_instance, l
-      #this almost never happens. If a connection is not established, unbind is called
-      #toplevel.connections.delete(l) if toplevel.connections[l].error?
     end
 
     def send_tcp_data(the_locspec)
@@ -40,14 +38,15 @@ module Bud
 
     def unbind
       @bud.connections_status[@locspec] = false
-      EventMachine.run {
-        EventMachine.add_timer(1) { 
-          puts "Scheduling a reconnect on #{@locspec[0]}:#{@locspec[1]}"
-          reconnect(@locspec[0], @locspec[1]) }
-      }
-      #@bud.connections.delete(@locspec)
+      @connected = false
+      unless @bud.connections_buffer[@locspec].nil? or @bud.connections_buffer[@locspec].empty?
+        EventMachine.run {
+          EventMachine.add_timer(1) { reconnect(@locspec[0], @locspec[1]) }
+        }
+      else
+        @bud.connections[@locspec].delete(@locspec)
+      end
     end
-
   end
 
 end
